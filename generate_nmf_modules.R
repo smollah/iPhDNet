f75b21e5142cb66c391a38efe1ed9ab41ed8fd51
smollah <smollah@eng.ucsd.edu>
2020-04-07 03:32:15 -0500

    Update generate_nmf_modules.R

--- generate_nmf_modules.R
+++ generate_nmf_modules.R
@@ -4,7 +4,8 @@
 # 
 # Generate functional modules using NMF
 #
-# S. A. Mollah and S. Subramaniam, “Histone Signatures Predict Therapeutic Efficacy in Breast Cancer”. 2020, IEEE Open Journal of Engineering in Medicine and Biology, vol. 1, pp. 74-82.
+# S. A. Mollah and S. Subramaniam, “Histone Signatures Predict Therapeutic Efficacy in Breast Cancer”. 
+# 2020, IEEE Open Journal of Engineering in Medicine and Biology, vol. 1, pp. 74-82.
 # (doi.org/10.1109/OJEMB.2020.2967105)
 #-----------------------------------------------------------------------------
 
