1af7c6df519ebe8429773ecfafb0e02ca3f3a072
bganglia <bganglia892@gmail.com>
2020-04-16 11:54:07 -0400

    Convert gct file to .txt

---
+++ preprocess_data.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 13 21:43:34 2020
+
+@author: compssd1
+"""
+
+import cmapPy.pandasGEXpress.parse
+
+in_out = [
+    ('LINCS_GCP_Plate27_annotated_minimized_2016-04-14_14-24-09.gct',
+     "60histon_24hr_final.txt"),
+    ("LINCS_P100_PRM_Plate29_24H_annotated_minimized_2016-01-28_17-11-22.gct",
+     "peptide_24hr_final.txt")
+]
+
+for in_, out in in_out:
+    df = cmapPy.pandasGEXpress.parse.parse(in_).data_df
+    df = df.apply(lambda x: x.fillna(x.mean()),axis=0)
+    df.to_csv(out,sep="\t")
+
+
